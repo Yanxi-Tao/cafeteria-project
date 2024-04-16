c08292e46ed1c4eda5176051bb602494b38dbb78
Candorada <guatavconrad@icloud.com>
2024-04-16 17:34:45 +0200

    test

--- app/page.jsx
+++ app/page.jsx
@@ -82,9 +82,9 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            <span className="inline-bloc transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
-            </span>
+            </span> 
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Explore starter templates for Next.js.
